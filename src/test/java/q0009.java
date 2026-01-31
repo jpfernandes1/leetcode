b55487ef8403d65e62ff04f93f92f8ca54688c76
jpfernandes1 <jp_fernandes1@live.com>
2026-01-31 17:33:06 -0300

    test: add the palindrome tests;

---
+++ src/test/java/q0009.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import q0009.PalindromeNumber;
+
+import java.util.stream.Stream;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+
+public class q0009 {
+
+   PalindromeNumber palindromeNumber = new PalindromeNumber();
+
+   static Stream<Arguments> vectorProvider() {
+       return Stream.of(
+             arguments(121, true),
+             arguments(-121, false),
+             arguments(10, false),
+             arguments(123, false)
+       );
+   }
+
+   @ParameterizedTest
+   @MethodSource("vectorProvider")
+   public void testPalindromeNumber(Integer number, boolean expected) {
+       boolean result = palindromeNumber.isPalindrome(number);
+       Assertions.assertEquals(expected, result);
+   }
+
+}
